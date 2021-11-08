4b21e9b4d9c7207dad79de971342f938d9893768
leeyunsik <leeys9932@naver.com>
2021-11-08 23:46:31 +0900

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI-Tutorial
+//
+//  Created by 이윤식 on 2021/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_TutorialApp.swift
+//  swiftUI-Tutorial
+//
+//  Created by 이윤식 on 2021/11/08.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
